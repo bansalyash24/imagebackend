c7a633c81a0a8b368d5b24392dde92cc44a598f5
bansalyash24 <95591146+bansalyash24@users.noreply.github.com>
2022-12-12 22:43:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(bodyParser.json({limit:'5000kb'}))
 
 require("dotenv").config();
 app.use(express.json());
-const port=5000;
+const port=process.env.PORT || 5000
 const userRoutes=require('./routes/userRoute');
 const imageRoute=require('./routes/imageRoute');
 app.use('/api/users',userRoutes)
